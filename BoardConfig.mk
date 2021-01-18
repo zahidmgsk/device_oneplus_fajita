ee90178e4188b486f47b8143fef986abcf76ad9b
chandu078 <chandudyavanapelli03@gmail.com>
2021-01-18 21:57:47 +0530

    fajita: bluetooth: Move bdroid header to sdm845-common Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,9 +22,6 @@ DEVICE_PATH := device/oneplus/fajita
 # Assert
 TARGET_OTA_ASSERT_DEVICE := OnePlus6T
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
-
 # FOD
 TARGET_SURFACEFLINGER_FOD_LIB := //$(DEVICE_PATH):libfod_extension.oneplus_fajita
 
