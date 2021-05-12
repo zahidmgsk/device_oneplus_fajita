86ee3c0f628b3c2af8f224c08b2d39c3251c3a89
zahidmgsk <zahidmgsk@gmail.com>
2021-05-12 09:00:58 +0000

    fajita: init AEX-R
    Signed-off-by: zahidmgsk <zahidmgsk@gmail.com>

--- aosp_fajita.mk
+++ aosp_fajita.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/fajita/device.mk)
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 
 # Boot Animation
