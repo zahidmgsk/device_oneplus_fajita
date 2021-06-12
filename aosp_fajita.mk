e0e1eb1b94c28b41c8652d4dfe0deaff0305f09b
author zahidmgsk <zahidmgsk@gmaiil.com>
2021-06-12 09:13:24 +0000

    fajita: Include Stock ARCore Signed-off-by: zahidmgsk <zahidmgsk@gmail.com>

--- aosp_fajita.mk
+++ aosp_fajita.mk
@@ -32,6 +32,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 #WITH_GAPPS := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_fajita
